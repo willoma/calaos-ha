6ecdf2631b8d97a7c01834195f497287956b19ff
Willow Maccagnoni <willow.maccagnoni@gmail.com>
2023-10-15 17:39:20 +0200

    reduce poll interval from 500ms to 250ms

--- custom_components/calaos/const.py
+++ custom_components/calaos/const.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 DOMAIN = "calaos"
 EVENT_DOMAIN = f"{DOMAIN}_event"
 
-POLL_INTERVAL = timedelta(seconds=0.5)
+POLL_INTERVAL = timedelta(seconds=0.25)
 
 INTEGER_MIN = -2147483648
 INTEGER_MAX = 2147483647
